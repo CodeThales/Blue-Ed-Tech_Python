aa7dbdec6408a89b0f33515f215a7da17fba81c6
Thales Ribeiro <thalles.agency@gmail.com>
2021-05-21 15:46:06 -0300

    indicação de key e value no dicionário

--- Aula 13 - Dicionários/Ex01.py
+++ Aula 13 - Dicionários/Ex01.py
@@ -2,4 +2,12 @@
 #os números 1, 4, 5, 6, 7, e 9 (que podem ser
 #armazenados em uma lista) e seus valores 
 #correspondentes aos quadrados desses números.
-valores = {1: 1, 4: 16, 5: 25, 6: 36, 7: 49, 9: 81}
+#1: 1, 4: 16, 5: 25, 6: 36, 7: 49, 9: 81
+valores = {}
+valores[1] = 1 ** 2
+valores[4] = 4 ** 2
+valores[5] = 5 ** 2
+valores[6] = 6 ** 2
+valores[7] = 7 ** 2
+valores[9] = 9 ** 2
+print(valores)
