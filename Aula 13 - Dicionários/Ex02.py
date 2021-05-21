fdbfe504ea5695bac55f7de631c6a1b566fb0e4c
Thales Ribeiro <thalles.agency@gmail.com>
2021-05-21 16:03:45 -0300

    inserindo valores exponenciais às keys do dicionário

--- Aula 13 - Dicionários/Ex02.py
+++ Aula 13 - Dicionários/Ex02.py
@@ -1,4 +1,16 @@
 #Crie um dicionário em que suas chaves 
 #correspondem a números inteiros entre 
 #[1, 10] e cada valor associado é o número ao quadrado.
-valores = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100} 
+#1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100 
+valores = {}
+valores[1] = 1 ** 2
+valores[2] = 2 ** 2
+valores[3] = 3 ** 2
+valores[4] = 4 ** 2
+valores[5] = 5 ** 2
+valores[6] = 6 ** 2
+valores[7] = 7 ** 2
+valores[8] = 8 ** 2
+valores[9] = 9 ** 2
+valores[10] = 10 ** 2
+print(valores)
