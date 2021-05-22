9bbee936143baf5bd131b768482d3c1c17b2adbe
Thales Ribeiro <thalles.agency@gmail.com>
2021-05-21 21:44:56 -0300

    Update Ex02.py
    Atualizado para uma versão bem mais elegante.

--- Aula 13 - Dicionários/Ex02.py
+++ Aula 13 - Dicionários/Ex02.py
@@ -2,15 +2,8 @@
 #correspondem a números inteiros entre 
 #[1, 10] e cada valor associado é o número ao quadrado.
 #1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100 
-valores = {}
-valores[1] = 1 ** 2
-valores[2] = 2 ** 2
-valores[3] = 3 ** 2
-valores[4] = 4 ** 2
-valores[5] = 5 ** 2
-valores[6] = 6 ** 2
-valores[7] = 7 ** 2
-valores[8] = 8 ** 2
-valores[9] = 9 ** 2
-valores[10] = 10 ** 2
-print(valores)
+numeros = [1,2,3,4,5,6,7,8,9,10]
+dicio = dict()
+for i in range(0, 10):    
+    dicio[numeros[i]] = numeros[i] ** 2
+print(dicio)
