6f0afb08aa9d70eccc8614ef3ab384601648babe
Thales Ribeiro <thalles.agency@gmail.com>
2021-05-21 21:28:31 -0300

    Update Ex01.py
    Uma versão mais bonita que aprendi na aula de hoje

--- Aula 13 - Dicionários/Ex01.py
+++ Aula 13 - Dicionários/Ex01.py
@@ -3,11 +3,11 @@
 #armazenados em uma lista) e seus valores 
 #correspondentes aos quadrados desses números.
 #1: 1, 4: 16, 5: 25, 6: 36, 7: 49, 9: 81
-valores = {}
-valores[1] = 1 ** 2
-valores[4] = 4 ** 2
-valores[5] = 5 ** 2
-valores[6] = 6 ** 2
-valores[7] = 7 ** 2
-valores[9] = 9 ** 2
-print(valores)
+lista = list()
+dados = {1: 0, 4: 0, 5: 0, 6: 0, 7: 0, 9: 0}
+lista.append(dados)
+for k in dados:
+    valor = k * k
+    for v in dados:
+        dados[k] = valor
+print(dados)
